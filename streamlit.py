afa007dd247c49baf6ba436aa294a6ab2b2ed4dd
Linda Adzigbli <106244819+Norkplim22@users.noreply.github.com>
2023-04-03 19:35:04 +0200

    up

--- streamlit.py
+++ streamlit.py
@@ -37,35 +37,15 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
 @st.cache_resource()  # stop the hot-reload to the function just bellow
 
-def setup(df):
-    "Setup the required elements like files, models, global variables, etc"
-    pd.DataFrame(
-        dict(
-            date=[],
-            onpromotion=[],
-            store_cluster=[],
-            family=[],
-            events=[],
-            oil_price=[],    
-        )
-    ).to_csv(df, index=False)
-    
-df = os.path.join(DIRPATH, "df.csv")
-setup(df)
- 
-
 #creating the interface
-
 st.image("https://ultimahoraec.com/wp-content/uploads/2021/03/dt.common.streams.StreamServer-768x426.jpg")
 st.title("Sales predictor app")
 
 st.caption("This app predicts sales patterns of Cooperation Favorita over time in different stores in Ecuador.")
 
-form = st.form(key="information", clear_on_submit=True)
-
-with form:
-
-    cols = st.columns((1, 1))
+input_df ={}
+cols = st.columns((1, 1))
+with cols:
     date=cols[0].date_input("date")
     store_cluster = cols[1].number_input("store_cluster", min_value=1,max_value=17,step=1)
     family = cols[0].selectbox("family:", ["AUTOMOTIVE", "BEAUTY AND FASHION", "BEVERAGES AND LIQUOR", "FROZEN FOODS", "Grocery", "HOME AND KITCHEN", "HOME CARE AND GARDEN", "PET SUPPLIES", "SCHOOL AND OFFICE SUPPLIES"], index=2)
@@ -78,35 +58,27 @@ with form:
     
 
 if submitted:
-    pd.read_csv(df).append(
-        dict(
-            date=date,
-            onpromotion=onpromotion,
-            store_cluster=store_cluster,
-            family=family,
-            events=events,
-            oil_price=oil_price,
-            ),
-        ignore_index=True,
-    ).to_csv(df, index=False)
-    df = pd.read_csv(df)
+    df = pd.dataframe(input_df)
+#transform date
     new_df = date_transform(df,date)
     
-    #Encoding on categorical columns.
-    cat = ["family", "events"]
-    num = ["onpromotion", "store_cluster", "oil_price"]
+    #Define columns
+    cat = [col for col in new_df.columns if new_df[col].dtype == 'object']
+    num = [col for col in new_df.columns if new_df[col].dtype != 'object']
     
+   #Encoding on categorical columns.  
     encoder = OneHotEncoder(drop = "first", sparse=False)
-    encoded_features = encoder.fit_transform(df[cat])
-    encoded_cat_col = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out().tolist())
+    encoded_features = encoder.transform(df[cat])
+    encoded_cat_col = pd.DataFrame(encoded_features.toarray(), columns=encoder.get_feature_names_out().tolist())
     new_df= pd.concat([new_df.reset_index(drop=True), encoded_cat_col], axis=1)
 
-    Prediction = decision_tree.predict(new_df)
+    Prediction = decision_tree.predict(new_df)[0]
     st.subheader(f"The prediction is {Prediction}")
+    
 
 st.balloons()
 
 expander = st.expander("See all records")
 with expander:
-    df_new = pd.read_csv(df)
+    df_new = pd.read_csv(new_df)
     st.dataframe(df_new)
